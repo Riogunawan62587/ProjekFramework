6c3b9c2b7950a606c4d29dbb5ffa7ecbc3d9c250
hafiyann <55193476+hafiyann@users.noreply.github.com>
2020-12-31 23:50:56 +0700

    uddate model

--- app/Reservation.php
+++ app/Reservation.php
@@ -11,6 +11,4 @@ class Reservation extends Model
     protected $fillable = [
         'tanggal','jam','jumlah_orang','status','table_id','user_id'
     ];
-
-    protected $table = 'reservations';
 }
