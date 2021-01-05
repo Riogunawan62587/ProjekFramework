3c1868dbd89fa68d9281fd21ccdaccc71fdfaf9b
hafiyann <55193476+hafiyann@users.noreply.github.com>
2021-01-05 17:48:31 +0700

    push

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \App\Http\Middleware\CheckRole::class,
         ],
 
         'api' => [
@@ -60,6 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'CheckRole' => \App\Http\Middleware\CheckRole::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Auth::routes();
 
 Route::get('/', function () {
   return view('index');
-});
+})->name('home');
 
 // Controller Home
 Route::get('/home', 'HomeController@index')->name('home.index');
@@ -36,36 +36,38 @@ Route::post('/reservasi_saya/delete', 'ReservationController@destroy')->name('re
 // Controller Artikel
 Route::get('/artikel', 'ArticleController@index')->name('artikel.index');
 
-// Controller Dashboard
-Route::get('/admin/dashboard', 'DashboardController@show_dashboard');
+Route::group(['middleware' => ['auth','CheckRole:Admin']], function(){
+  // Controller Dashboard
+  Route::get('/admin/dashboard', 'DashboardController@show_dashboard');
 
-Route::get('/admin/menu', 'MenuController@index_admin');
-Route::post('/admin/menu', 'MenuController@store');
-Route::post('/admin/menu/delete', 'MenuController@destroy');
-Route::post('admin/menu/edit', 'MenuController@edit');
-Route::post('admin/menu/update', 'MenuController@update');
+  Route::get('/admin/menu', 'MenuController@index_admin');
+  Route::post('/admin/menu', 'MenuController@store');
+  Route::post('/admin/menu/delete', 'MenuController@destroy');
+  Route::post('admin/menu/edit', 'MenuController@edit');
+  Route::post('admin/menu/update', 'MenuController@update');
 
-//  Controller Table
-Route::get('/admin/meja', 'TableController@index_admin');
-Route::post('/admin/meja', 'TableController@create');
-Route::post('/admin/meja/delete', 'TableController@destroy');
-Route::post('admin/meja/update', 'TableController@update');
+  //  Controller Table
+  Route::get('/admin/meja', 'TableController@index_admin');
+  Route::post('/admin/meja', 'TableController@create');
+  Route::post('/admin/meja/delete', 'TableController@destroy');
+  Route::post('admin/meja/update', 'TableController@update');
 
-Route::get('/admin/pengguna', 'UserController@users_list');
-Route::post('/admin/pengguna/detail', 'UserController@user_detail');
-Route::post('/admin/pengguna/hapus', 'UserController@destroy');
+  Route::get('/admin/pengguna', 'UserController@users_list');
+  Route::post('/admin/pengguna/detail', 'UserController@user_detail');
+  Route::post('/admin/pengguna/hapus', 'UserController@destroy');
 
 
-Route::get('/admin/reservasi', 'ReservationController@reservations_list');
+  Route::get('/admin/reservasi', 'ReservationController@reservations_list');
 
-Route::get('/admin/artikel', 'ArticleController@articles_list');
-Route::get('/admin/artikel/tambah','ArticleController@show_create');
-Route::post('/admin/artikel/tambah','ArticleController@add_article');
-Route::post('/admin/artikel/detail','ArticleController@article_detail');
-Route::post('/admin/artikel/edit','ArticleController@show_edit');
-Route::post('/admin/artikel/update','ArticleController@update_article');
-Route::post('/admin/artikel/delete','ArticleController@destroy');
+  Route::get('/admin/artikel', 'ArticleController@articles_list');
+  Route::get('/admin/artikel/tambah','ArticleController@show_create');
+  Route::post('/admin/artikel/tambah','ArticleController@add_article');
+  Route::post('/admin/artikel/detail','ArticleController@article_detail');
+  Route::post('/admin/artikel/edit','ArticleController@show_edit');
+  Route::post('/admin/artikel/update','ArticleController@update_article');
+  Route::post('/admin/artikel/delete','ArticleController@destroy');
 
-Route::post('/admin/reservasi/detail', 'ReservationController@reservation_detail');
-Route::post('/admin/reservasi','ReservationController@reservation_change_status');
-Route::post('/admin/reservasi/hapus','ReservationController@destroy_reservation_admin');
+  Route::post('/admin/reservasi/detail', 'ReservationController@reservation_detail');
+  Route::post('/admin/reservasi','ReservationController@reservation_change_status');
+  Route::post('/admin/reservasi/hapus','ReservationController@destroy_reservation_admin');
+});
